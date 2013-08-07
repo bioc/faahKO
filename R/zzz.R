e6580c7bc2360895279de19ab16eb02c0c6f5860
Dan Tenenbaum <dtenenba@fhcrc.org>
2013-08-07 05:47:55 +0000

    call find.package() instead of .find.package()
    git-svn-id: file:///home/git/bioc-data.hedgehog.fhcrc.org/trunk/experiment/pkgs/faahKO@2417 db2202da-8704-0410-b924-b125b3cd84ef

--- R/zzz.R
+++ R/zzz.R
@@ -1,2 +1,2 @@
-load(file.path(.find.package("faahKO"), "data", "faahko.rda"))
+load(file.path(find.package("faahKO"), "data", "faahko.rda"))
 attr(faahko, "filepaths") <- file.path(find.package("faahKO"), gsub("/", .Platform$file.sep, attr(faahko, "filepaths")))
