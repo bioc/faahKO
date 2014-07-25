2bb393110c3c5b9d8885f55b63ea722bb7a8c019
Dan Tenenbaum <dtenenba@fhcrc.org>
2014-07-25 23:52:56 +0000

    use system.file, not find.package
    git-svn-id: file:///home/git/bioc-data.hedgehog.fhcrc.org/branches/RELEASE_2_14/experiment/pkgs/faahKO@2958 db2202da-8704-0410-b924-b125b3cd84ef

--- R/zzz.R
+++ R/zzz.R
@@ -1,2 +1,2 @@
 load(file.path(find.package("faahKO"), "data", "faahko.rda"))
-attr(faahko, "filepaths") <- file.path(find.package("faahKO"), gsub("/", .Platform$file.sep, attr(faahko, "filepaths")))
+attr(faahko, "filepaths") <- file.path(system.file(package="faahKO"), gsub("/", .Platform$file.sep, attr(faahko, "filepaths"))
