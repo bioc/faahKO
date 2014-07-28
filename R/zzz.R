82787c1643d4af6d4c0b4c1f1c187e62cde65de1
Dan Tenenbaum <dtenenba@fhcrc.org>
2014-07-28 20:11:16 +0000

    set filepaths(faahko) to include path to installed faahKO, on package load.
    git-svn-id: file:///home/git/bioc-data.hedgehog.fhcrc.org/trunk/experiment/pkgs/faahKO@2959 db2202da-8704-0410-b924-b125b3cd84ef

--- R/zzz.R
+++ R/zzz.R
@@ -1,2 +1,3 @@
 load(file.path(find.package("faahKO"), "data", "faahko.rda"))
-attr(faahko, "filepaths") <- file.path(system.file(package="faahKO"), gsub("/", .Platform$file.sep, attr(faahko, "filepaths"))
+filepaths(faahko) <- file.path(system.file(package="faahKO"),
+   gsub("/", .Platform$file.sep, attr(faahko, "filepaths")))
