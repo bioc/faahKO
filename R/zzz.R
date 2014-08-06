ab69aa5b2b3bb17f2a3806084f37fc805bda76b5
Dan Tenenbaum <dtenenba@fhcrc.org>
2014-08-06 18:33:58 +0000

    filepaths to faahko object are determined at load time, not build time (thanks to Valerie for the hint)
    git-svn-id: file:///home/git/bioc-data.hedgehog.fhcrc.org/branches/RELEASE_2_14/experiment/pkgs/faahKO@2966 db2202da-8704-0410-b924-b125b3cd84ef

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,8 @@
-load(file.path(find.package("faahKO"), "data", "faahko.rda"))
-filepaths(faahko) <- file.path(system.file(package="faahKO"),
-   gsub("/", .Platform$file.sep, attr(faahko, "filepaths")))
+.onLoad <- function(libname, pkgname)
+{
+     load(file.path(find.package("faahKO"), "data", "faahko.rda"))
+     nms <- file.path(system.file(package="faahKO"),
+         gsub("/", .Platform$file.sep, attr(faahko, "filepaths")))
+     filepaths(faahko) <- nms
+     assign("faahko", faahko, env = globalenv())
+}
