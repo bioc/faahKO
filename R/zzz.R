4f10de78059d37bc55065367b64ca7a8b11ae42a
Dan Tenenbaum <dtenenba@fhcrc.org>
2014-08-18 22:18:24 +0000

    make sure xcms is loaded before we call filepaths()
    git-svn-id: file:///home/git/bioc-data.hedgehog.fhcrc.org/trunk/experiment/pkgs/faahKO@2978 db2202da-8704-0410-b924-b125b3cd84ef

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,6 @@
 .onLoad <- function(libname, pkgname)
 {
+     suppressPackageStartupMessages(require(xcms))
      load(file.path(find.package("faahKO"), "data", "faahko.rda"))
      nms <- file.path(system.file(package="faahKO"),
          gsub("/", .Platform$file.sep, attr(faahko, "filepaths")))
