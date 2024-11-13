c264d11751ed381dbf83d63327c48828d873d2cf
Steffen Neumann <sneumann@ipb-halle.de>
2024-11-13 15:57:37 +0100

    Improve importing from xcms to adapt filepaths() to local installation, plus cleanups

--- R/zzz.R
+++ R/zzz.R
@@ -1,9 +1,8 @@
 .onLoad <- function(libname, pkgname)
 {
-     suppressPackageStartupMessages(require(xcms))
      load(file.path(find.package("faahKO"), "data", "faahko.rda"))
      nms <- file.path(system.file(package="faahKO"),
          gsub("/", .Platform$file.sep, attr(faahko, "filepaths")))
      filepaths(faahko) <- nms
-     assign("faahko", faahko, env = getNamespace("faahKO"))
+     assign("faahko", faahko, envir = getNamespace("faahKO"))
 }
